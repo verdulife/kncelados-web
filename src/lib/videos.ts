8b428c68fe56640942849b504f5209ba85352da0
verdulife <verdu@live.com>
2023-11-19 21:55:07 +0100

    joz done

---
+++ src/lib/videos.ts
@@ -0,0 +1,14 @@
+export const videos = [
+  {
+    url: "https://www.tiktok.com/@kncelados/video/7298378404989062432",
+    id: "7298378404989062432"
+  },
+  {
+    url: "https://www.tiktok.com/@kncelados/video/7302513761162775841",
+    id: "7302513761162775841"
+  },
+  {
+    url: "https://www.tiktok.com/@kncelados/video/7293204897581911328",
+    id: "7293204897581911328"
+  },
+]
