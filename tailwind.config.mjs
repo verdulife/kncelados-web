3ce80b4d9f3c8c43f2511be693c9f13d6fdf93fa
verdulife <verdu@live.com>
2023-11-20 18:40:09 +0100

    reveal fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      height: {
+        screen: "100dvh",
+      },
       fontFamily: {
-        title: ["Everett", ...defaultTheme.fontFamily.sans],
+        title: [
+          "Everett",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Apple Color Emoji",
+          ...defaultTheme.fontFamily.sans,
+        ],
       },
       colors: {
         knred: "#e13300",
