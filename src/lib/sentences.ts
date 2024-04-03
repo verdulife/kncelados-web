16138b5eb17169a8e32f99fca47f9853561fc42b
verdulife <verdu@live.com>
2024-04-04 01:22:25 +0200

    test final

--- src/lib/sentences.ts
+++ src/lib/sentences.ts
@@ -1,3 +1,5 @@
 export const sentences = [
   "¿Como le dice un mudo a un ciego que se ha ido la luz?",
+  "Pero es más rápido si lo doblas",
+  "Pepa, para Pepin pon pan...",
 ];
--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -1,12 +1,4 @@
-export type EPISODE_TYPE = {
-  episode: number;
-  season: number;
-  id: string;
-};
-
-export type SEASON_TYPE = Array<Array<EPISODE_TYPE>>;
-
-export const episodes: SEASON_TYPE = [
+export const episodes = [
   [
     {
       episode: 1,
@@ -38,25 +30,61 @@ export const episodes: SEASON_TYPE = [
       season: 1,
       id: "xEY_IpEqqGM",
     },
+    {
+      episode: 4,
+      season: 1,
+      id: "usZudg4T0I0",
+    },
+    {
+      episode: 5,
+      season: 1,
+      id: "DfYNW0Na2Gg",
+    },
+    {
+      episode: 6,
+      season: 1,
+      id: "qHQgi7vkHIM",
+    },
+    {
+      episode: 7,
+      season: 1,
+      id: "wVstFgePIFY",
+    },
+    {
+      episode: 8,
+      season: 1,
+      id: "pan35TQxtGk",
+    },
+    {
+      episode: 9,
+      season: 1,
+      id: "WZjC27vS6RA",
+    },
+    {
+      episode: 10,
+      season: 1,
+      id: "PjD1CEdz_ZE",
+    },
   ],
   [
     {
       episode: 1,
       season: 2,
-      id: "Iw6uTW_CWoM",
+      id: "8MIZIwr_2I4",
     },
     {
       episode: 2,
       season: 2,
-      id: "ulOpNfz64gA",
+      id: "94MaHry1ptA",
     },
     {
       episode: 3,
       season: 2,
-      id: "Iw6uTW_CWoM",
+      id: "6gCCQ58cqhY",
     },
   ],
 ];
 
-export const last_season_index = episodes.length - 1;
-export const last_episode_index = episodes[last_season_index].length - 1;
+const last_season_index = episodes.length - 1;
+const last_episode_index = episodes[last_season_index].length - 1;
+export const last_episode = episodes[last_season_index][last_episode_index];
