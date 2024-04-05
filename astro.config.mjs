b8978677ee573514a2d85c2d216aee1f4d395270
verdulife <verdu@live.com>
2024-04-05 10:27:40 +0200

    add partytown 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-
 import partytown from "@astrojs/partytown";
+
+const pt_opts = {
+  config: {
+    forward: ["dataLayer.push"],
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  integrations: [tailwind(), partytown(pt_opts)],
   output: "server",
   adapter: netlify()
 });
