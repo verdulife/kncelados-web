d41ac7f952733379fa7330a95692442c2d3fb323
verdulife <verdu@live.com>
2024-04-09 00:43:57 +0200

    netflix init

--- src/lib/sentences.ts
+++ src/lib/sentences.ts
@@ -2,5 +2,6 @@ export const sentences = [
   "¿Como le dice un mudo a un ciego que se ha ido la luz?",
   "Pero es más rápido si lo doblas",
   "Pepa, para Pepin pon pan...",
-  "KN Coches, ¿te gusta conducir?"
+  "KN Coches, ¿te gusta conducir?",
+  "Ligar... Pues eres tu mismo y rezas"
 ];
