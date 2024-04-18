a26e3403df3881660f0a055aac716ad72d20bfb3
verdulife <verdu@live.com>
2024-04-18 16:27:33 +0200

    get last scrape logic

--- scrape/getLast.js
+++ scrape/getLast.js
@@ -1,37 +1,35 @@
+import { episodes } from "./episodes.js";
+import { remakeDescription } from "./remake.js";
 import { writeFile } from "node:fs/promises";
 import ogs from "open-graph-scraper";
-import { episodes } from "./episodes.js";
 
 const BASE_URL = "https://www.youtube.com/watch?v=";
-let data = [];
+const last_episode = episodes[0][0];
+const { episode, season, id } = last_episode;
+const options = { url: BASE_URL + id };
 
-for await (let [ind, seasonData] of episodes.entries()) {
-  data[ind] = [];
+try {
+  const res = await ogs(options);
+  const { ogTitle, ogDescription, ogImage, requestUrl } = res.result;
+  const description = await remakeDescription(ogDescription);
 
-  for await (let episodeData of seasonData) {
-    const { episode, season, id } = episodeData;
-    const options = { url: BASE_URL + id };
+  console.log(description);
 
-    try {
-      const res = await ogs(options);
-      const { ogTitle, ogDescription, ogImage, requestUrl } = res.result;
-
-      data[ind].push({
-        episode,
-        season,
-        id,
-        title: ogTitle,
-        description: ogDescription,
-        image: ogImage[0].url,
-        url: requestUrl
-      });
-
-      console.log(`Making ${season}x${episode}`);
-    } catch (error) {
-      console.error(error);
-    }
+  const data = {
+    episode,
+    season,
+    id,
+    title: ogTitle,
+    description: ogDescription,
+    image: ogImage[0].url,
+    url: requestUrl
   };
-}
 
-const json = JSON.stringify(data, null, 2);
-await writeFile("./scrape/episodes.json", json, "utf-8")
+  console.log(`Making ${season}x${episode}`);
+  /* const json = JSON.stringify(data, null, 2);
+  await writeFile("./scrape/episodes.json", json, "utf-8"); */
+
+  console.log(data);
+} catch (error) {
+  console.error(error);
+}
--- scrape/remake.js
+++ scrape/remake.js
@@ -1,33 +1,36 @@
 import { CohereClient } from "cohere-ai";
-import episodes from "./episodes.json" with { type: "json" };
 
 const cohere = new CohereClient({
   token: "PhYsAmyxZ50A5WKnYmaaN3uICnWsrhVnzsSJZpvs"
 });
 
-const { text } = await cohere.chat({
-  chatHistory: [
-    {
-      role: "USER",
-      message: `
-        Tengo una descripcion de un video de youtube, puedes modificarla para que sea la misma, pero que google no lo reconozca como texto duplicado.
-        Algunas consideraciones son:
-          - Kncelados es un video podcast de humor.
-          - El podcast nunca ofrece consejos ni trucos, solo humor y tonterias.
-          - Kncelados es un nombre propio.
-          - Para los nombres propios nunca hagas modificaciones.
-          - Tampoco necesito los hashtags.
-          - Tambien necesito que el tono el texto, sea desenfadado, pero que no sea recargado ni ñoño.
-          - Kncelados es un podcast de varios integrantes, refierete a Kncelados en plural.
-          - Responde exclusivamente la respuesta, no añadas texto introductorio ni minesajes sobre el trabajo realizado.
-          - Tampoco embuelvas la respuesta entre comitas.
-          - Devuelte un texto en primera persona (como si fueras el podcast Kncelados).
-          - Habla tuteando, con español de España.
-          - No generes textos de cierre.
-      `
-    }
-  ],
-  message: episodes[0][0].description
-});
+export async function remakeDescription(title, description) {
+  const { text } = await cohere.chat({
+    chatHistory: [
+      {
+        role: "USER",
+        message: `
+          Tengo una descripcion de un video de youtube, puedes modificarla para que sea la misma, pero que google no lo reconozca como texto duplicado.
+          Algunas consideraciones son:
+            - Kncelados es un video podcast de humor.
+            - El podcast nunca ofrece consejos ni trucos, solo humor y tonterias.
+            - Kncelados es un nombre propio.
+            - Para los nombres propios nunca hagas modificaciones.
+            - Tampoco necesito los hashtags.
+            - Tambien necesito que el tono el texto, sea desenfadado, pero que no sea recargado ni ñoño.
+            - Kncelados es un podcast de varios integrantes, refierete a Kncelados en plural.
+            - Responde exclusivamente la respuesta, no añadas texto introductorio ni minesajes sobre el trabajo realizado.
+            - Tampoco embuelvas la respuesta entre comitas.
+            - Devuelte un texto en primera persona (como si fueras el podcast Kncelados).
+            - Habla tuteando, con español de España.
+            - No generes textos de cierre.
+            - Segun el titulo "${title}", y considerando la description que generes, reforumala tambien el titulo.
+            - devuelveme el titulo y la descripcion en un array, donde la posicion 0 sea el titulo y la 1 la descripcion.
+        `
+      }
+    ],
+    message: description
+  });
 
-console.log(text);
+  return text;
+}
