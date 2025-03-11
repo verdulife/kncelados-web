d683244c30eef6f44dcc57ac609a0da276b52a39
verdulife <verdu@live.com>
2025-03-11 16:11:32 +0100

    update all refs

--- mystery/generateQr.js
+++ mystery/generateQr.js
@@ -2,8 +2,10 @@ import QRCode from "qrcode";
 import { videos } from "./mystery.js";
 
 videos.forEach(({ slug, page }) => {
+  console.log("Generando QRCode para", slug);
+
   QRCode.toFile(
-    `qr/svg/${page}_${slug}.svg`,
+    `mystery/svg/${page}_${slug}.svg`,
     `https://www.kncelados.com/mystery/${slug}`,
     { type: "svg" }
   );
