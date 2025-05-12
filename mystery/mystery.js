01f7b372e9aa1409d5b14458609555cb0ddcf64b
verdulife <verdu@live.com>
2025-05-12 18:47:42 +0200

    add more mysteries

--- mystery/mystery.js
+++ mystery/mystery.js
@@ -51,13 +51,13 @@ export const videos = [
     title: "La lepisma hambrienta",
     page: "32",
     slug: "la-lepisma-hambrienta",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/KmZQzobgLp4"
   },
   {
     title: "Fastan furius",
     page: "33",
     slug: "fastan-furius",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/HZ8I49fM48k"
   },
   {
     title: "El torneo de artes marciales",
@@ -135,19 +135,19 @@ export const videos = [
     title: "Espejo espejito",
     page: "48",
     slug: "espejo-espejito",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/7X_NF7Eo1_E"
   },
   {
     title: "Corrindando",
     page: "49",
     slug: "corrindando",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/51Oao-bu6Iw"
   },
   {
     title: "Aventura en el amazonas",
     page: "50",
     slug: "aventura-en-el-amazonas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/qhrmAdoaxoU"
   },
   {
     title: "¡Ni un paso atrás!",
@@ -165,25 +165,25 @@ export const videos = [
     title: "Puertas y más puertas",
     page: "53",
     slug: "puertas-y-mas-puertas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/1TB2bTrxcsE"
   },
   {
     title: "Intersección temporal",
     page: "54",
     slug: "interseccion-temporal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/hKhzQXrm99g"
   },
   {
     title: "HMS",
     page: "54",
     slug: "hms",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/PCvg9AGF3OQ"
   },
   {
     title: "Plata o plomo",
     page: "55",
     slug: "plata-o-plomo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Dkm3w9o3q0s"
   },
   {
     title: "Hospedaje mortal",
@@ -195,7 +195,7 @@ export const videos = [
     title: "Control gravitatorio",
     page: "58",
     slug: "control-gravitatorio",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/3wUlj4-JSvg"
   },
   {
     title: "Últimos cartuchos",
@@ -207,19 +207,19 @@ export const videos = [
     title: "Resacón en Murcia",
     page: "60",
     slug: "resacon-en-murcia",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/pUu8-S0T_3M"
   },
   {
     title: "Árbol genealógico",
     page: "61",
     slug: "arbol-genealogico",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/M8QB-BxJPgo"
   },
   {
     title: "Me lo quitan de las manos",
     page: "61",
     slug: "me-lo-quitan-de-las-manos",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/lY9zX2B28fk"
   },
   {
     title: "Robo blindado",
@@ -231,37 +231,37 @@ export const videos = [
     title: "Supervivencia glacial",
     page: "64",
     slug: "supervivencia-glacial",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/U0dLVHM4fsw"
   },
   {
     title: "Cumpleaños mágico",
     page: "65",
     slug: "cumpleanos-magico",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/9lm1EFMzdJ4"
   },
   {
     title: "Thiago el mentalista",
     page: "66",
     slug: "thiago-el-mentalista",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/KprwP8eUCwo"
   },
   {
     title: "Esquizofrenia canina",
     page: "67",
     slug: "esquizofrenia-canina",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/cvt-JN37VXQ"
   },
   {
     title: "Road Trip",
     page: "68",
     slug: "road-trip",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Uj0vyIflons"
   },
   {
     title: "Linaje dudoso",
     page: "69",
     slug: "linaje-dudoso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5UIR2P4L2M8"
   },
   {
     title: "Entrega urgente",
@@ -273,73 +273,73 @@ export const videos = [
     title: "La rica manzana",
     page: "72",
     slug: "la-rica-manzana",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/vohDW5Tay1M"
   },
   {
     title: "Atracón vegano",
     page: "73",
     slug: "atracon-vegano",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/FHndRUa2Svc"
   },
   {
     title: "Vinland",
     page: "74",
     slug: "vinland",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/BrtA_HiJ6Uk"
   },
   {
     title: "Todo al rojo, no ?",
     page: "75",
     slug: "todo-al-rojo-no",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/u9K8TuOzMCs"
   },
   {
     title: "Misterio bifurcal",
     page: "76",
     slug: "misterio-bifurcal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/c3Hhv5878P0"
   },
   {
     title: "Doble o nada",
     page: "77",
     slug: "doble-o-nada",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/mC3uqCcFbPo"
   },
   {
     title: "Entre la espada y la pared",
     page: "78",
     slug: "entre-la-espada-y-la-pared",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/NGE32vTpOKM"
   },
   {
     title: "Bingo especial",
     page: "79",
     slug: "bingo-especial",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/19peDpJzuNc"
   },
   {
     title: "El corsario dorado",
     page: "80",
     slug: "el-corsario-dorado",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/2-c7lzDpkwQ"
   },
   {
     title: "Prison Break",
     page: "81",
     slug: "prison-break",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/49W5N6jV98Q"
   },
   {
     title: "Becarios no",
     page: "82",
     slug: "becarios-no",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/VFcmL05DK2k"
   },
   {
     title: "Días cortos",
     page: "83",
     slug: "dias-cortos",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/TUqXrTsovkQ"
   },
   {
     title: "Crimen en la floristería",
@@ -357,7 +357,7 @@ export const videos = [
     title: "Últimos coletazos",
     page: "87",
     slug: "ultimos-coletazos",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/PMz96LDv8r8"
   },
   {
     title: "Cuando aprieta el calor",
@@ -369,468 +369,468 @@ export const videos = [
     title: "El hombre milagro",
     page: "90",
     slug: "el-hombre-milagro",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/xwhuZLS3z7w"
   },
   {
     title: "La gran odisea",
     page: "91",
     slug: "la-gran-odisea",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/fkvbu7WH9VQ"
   },
   {
     title: "La mano inocente",
     page: "92",
     slug: "la-mano-inocente",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/8HFFzte1D5E"
   },
   {
     title: "El retrato de Thiago Gray",
     page: "93",
     slug: "el-retrato-de-thiago-gray",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/KDPLSvZuZQM"
   },
   {
     title: "Los Diablos de Albacete",
     page: "94",
     slug: "los-diablos-de-albacete",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/jdL3S4IcWDI"
   },
   {
     title: "Desajuste temporal",
     page: "95",
     slug: "desajuste-temporal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/WRdEapikOFs"
   },
   {
     title: "Compro oro",
     page: "96",
     slug: "compro-oro",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Qn3CL6VJXgY"
   },
   {
     title: "Robo venenoso",
     page: "97",
     slug: "robo-venenoso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/HKEUmwCz2BI"
   },
   {
     title: "Sistema métrico",
     page: "98",
     slug: "sistema-metrico",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/oHlTia_pjFQ"
   },
   {
-    title: "¿Dónde está la bolita ?",
+    title: "¿Dónde está la bolita?",
     page: "99",
     slug: "donde-esta-la-bolita",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/hQ9Kh6a40Ho"
   },
   {
     title: "Vuelo accidentado",
     page: "100",
     slug: "vuelo-accidentado",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5dZDoN_XFEc"
   },
   {
     title: "Fantasma aéreo",
     page: "102",
     slug: "fantasma-aereo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/li5_qizch_Q"
   },
   {
     title: "Catástrofe ecuestre",
     page: "103",
     slug: "catastrofe-ecuestre",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/kxsOCDrkDUE"
   },
   {
     title: "Los jardines de Babilonia",
     page: "104",
     slug: "los-jardines-de-babilonia",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/kjptoFByqxI"
   },
   {
     title: "El terror del condado",
     page: "105",
     slug: "el-terror-del-condado",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/lwr1762KvAs"
   },
   {
     title: "La casa maldita",
     page: "106",
     slug: "la-casa-maldita",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/7OgnxcrKEb0"
   },
   {
     title: "Ángulo agudo",
     page: "107",
     slug: "angulo-agudo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/b2sXSwGTKww"
   },
   {
     title: "Robo animal",
     page: "108",
     slug: "robo-animal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5GQyB63ti4w"
   },
   {
     title: "Conquistador Fantasmus",
     page: "109",
     slug: "conquistador-fantasmus",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/c9__Eo1tAIw"
   },
   {
     title: "Robo bífido",
     page: "110",
     slug: "robo-bifido",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/WnsbWwd85KA"
   },
   {
     title: "Sin blanca",
     page: "112",
     slug: "sin-blanca",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/RqIaUxkavdw"
   },
   {
     title: "Excursión de gavilanes",
     page: "113",
     slug: "excursion-de-gavilanes",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/LXWSXJ7-36o"
   },
   {
     title: "Braguetazo",
     page: "114",
     slug: "braguetazo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/-Tx8tEQ7SBk"
   },
   {
     title: "Desayuno nutritivo",
     page: "115",
     slug: "desayuno-nutritivo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/POxEpGGQR4k"
   },
   {
     title: "Horneando en familia",
     page: "116",
     slug: "horneando-en-familia",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/FftnEShbyv0"
   },
   {
     title: "Salto temporal",
     page: "117",
     slug: "salto-temporal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/lqXm8ag69Jg"
   },
   {
     title: "Sin lubricante no, gracias",
     page: "120",
     slug: "sin-lubricante-no-gracias",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/OXstkrxkTkk"
   },
   {
     title: "Un trabajo de hormigas",
     page: "122",
     slug: "un-trabajo-de-hormigas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/hId14U2yKnA"
   },
   {
     title: "No me toques las campanas",
     page: "123",
     slug: "no-me-toques-las-campanas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/eolHw236wAQ"
   },
   {
     title: "Das sueldo",
     page: "124",
     slug: "das-sueldo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Bv5w-PxbtNw"
   },
   {
     title: "Cruce amoroso",
     page: "125",
     slug: "cruce-amoroso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/eU7iNREU4RQ"
   },
   {
     title: "Separación de bienes",
     page: "126",
     slug: "separacion-de-bienes",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/d9fzP6uqP2k"
   },
   {
     title: "Cosas de campo",
     page: "127",
     slug: "cosas-de-campo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/FGenJk63zD4"
   },
   {
     title: "Al calor de la tarta",
     page: "128",
     slug: "al-calor-de-la-tarta",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/YsM96fGKHK0"
   },
   {
     title: "Infracción al volante",
     page: "129",
     slug: "infraccion-al-volante",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/qBsPEli9l64"
   },
   {
     title: "Mordiendo el anzuelo",
     page: "130",
     slug: "mordiendo-el-anzuelo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/tB4d1WpqIPQ"
   },
   {
     title: "El juego de la botella",
     page: "131",
     slug: "el-juego-de-la-botella",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5soqp-6Sk68"
   },
   {
     title: "Robo mentiroso",
     page: "132",
     slug: "robo-mentiroso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Hx7TZ4qwwqw"
   },
   {
     title: "Pago en cadena",
     page: "133",
     slug: "pago-en-cadena",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/n4giqWoOAZg"
   },
   {
     title: "Confusión pasional",
     page: "134",
     slug: "confusion-pasional",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/9KWjxJe9tWY"
   },
   {
     title: "Plan B",
     page: "135",
     slug: "plan-b",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/VKoGw5ks-ZY"
   },
   {
     title: "La boina es de inteligente",
     page: "136",
     slug: "la-boina-es-de-inteligente",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/hPz_p6Br3ZA"
   },
   {
     title: "Traición mafiosa",
     page: "137",
     slug: "traicion-mafiosa",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/ybOldOSI2so"
   },
   {
     title: "Hasta luego",
     page: "138",
     slug: "hasta-luego",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/YZFcvBFwSNA"
   },
   {
     title: "Una cuestión de peso",
     page: "139",
     slug: "una-cuestion-de-peso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/7qjdapNUsok"
   },
   {
     title: "Hasta ahora",
     page: "140",
     slug: "hasta-ahora",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/MJTijJBppjA"
   },
   {
     title: "Geometría básica",
     page: "142",
     slug: "geometria-basica",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5UDf3L-X3jE"
   },
   {
     title: "Gunther el relojero",
     page: "143",
     slug: "gunther-el-relojero",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/9UcPiVgA7Us"
   },
   {
     title: "Fuga fantasma",
     page: "144",
     slug: "fuga-fantasma",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/ZLlDWxQ49ps"
   },
   {
     title: "Peleas clandestinas",
     page: "145",
     slug: "peleas-clandestinas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/9W9bzVHsmMg"
   },
   {
     title: "Piedra, papel, tijeras, lagarto, spock",
     page: "146",
     slug: "piedra-papel-tijeras-lagarto-spock",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/NnMG0Gtekdw"
   },
   {
     title: "Aventura mágica",
     page: "147",
     slug: "aventura-magica",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/JP207_1Zqa8"
   },
   {
     title: "Quemando calorías",
     page: "148",
     slug: "quemando-calorias",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/efr5Ue_eOKw"
   },
   {
     title: "Regateo frutal",
     page: "149",
     slug: "regateo-frutal",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/WKOcwYUIydM"
   },
   {
     title: "Es una fieshta",
     page: "150",
     slug: "es-una-fieshta",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/2NBQI7RxbfU"
   },
   {
     title: "Dibújame en el código",
     page: "152",
     slug: "dibujame-en-el-codigo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/aupcOg6nuTk"
   },
   {
     title: "Paseo familiar",
     page: "153",
     slug: "paseo-familiar",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/HqJ0ueB-tic"
   },
   {
     title: "Abecedario en serie",
     page: "154",
     slug: "abecedario-en-serie",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/D8xssaY4bZc"
   },
   {
     title: "Te elijo a ti, Mona Lisa",
     page: "155",
     slug: "te-elijo-a-ti-mona-lisa",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/gNAK-mZCmvk"
   },
   {
     title: "Pistas binarias",
     page: "156",
     slug: "pistas-binarias",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/1jh_e249ptY"
   },
   {
     title: "Chorizeo incompetente",
     page: "157",
     slug: "chorizeo-incompetente",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/hLIATBdF0kg"
   },
   {
     title: "Dinero y Apu en las Vegas",
     page: "158",
     slug: "dinero-y-apu-en-las-vegas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/H8X7lPKaI6w"
   },
   {
     title: "Vendedor Perignon",
     page: "159",
     slug: "vendedor-perignon",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/xX7TiP0rR64"
   },
   {
     title: "No hay honor entre rateros",
     page: "160",
     slug: "no-hay-honor-entre-rateros",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/5VUuMK_QhZQ"
   },
   {
     title: "Guantazo en la oscuridad",
     page: "161",
     slug: "guantazo-en-la-oscuridad",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/A3F6dPnCLtw"
   },
   {
     title: "Me falta algo",
     page: "162",
     slug: "me-falta-algo",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/zPiLINQL75w"
   },
   {
     title: "Construcción Romana",
     page: "164",
     slug: "construccion-romana",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/lw6HKdb0ysM"
   },
   {
     title: "Velas en la oscuridad",
     page: "165",
     slug: "velas-en-la-oscuridad",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/Qe055U2syKM"
   },
   {
     title: "Renacimiento",
     page: "166",
     slug: "renacimiento",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/kWO0FcQmd_c"
   },
   {
     title: "Spain is diferent",
     page: "167",
     slug: "spain-is-diferent",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/zaRV1F8RQM4"
   },
   {
     title: "Inventor excéntrico",
     page: "168",
     slug: "inventor-excentrico",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/LOwSp9uTLdY"
   },
   {
     title: "Mentiras en tierras lejanas",
     page: "169",
     slug: "mentiras-en-tierras-lejanas",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/tf7I74SM1gk"
   },
   {
     title: "En busca del delincuente",
     page: "170",
     slug: "en-busca-del-delincuente",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/IvHrD8w3Y78"
   },
   {
     title: "Movimiento sigiloso",
     page: "174",
     slug: "movimiento-sigiloso",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/_dacJQyEIoI"
   },
   {
     title: "Expedición Arcana",
     page: "175",
     slug: "expedicion-arcana",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/wEloN54SAG0"
   },
   {
     title: "Escapada corrupta",
     page: "176",
     slug: "escapada-corrupta",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/dsNOSB6VQ4Y"
   },
   {
     title: "Juego de máscaras",
     page: "177",
     slug: "juego-de-mascaras",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/0RawtP0m9es"
   },
   {
     title: "Plot Twist",
     page: "178",
     slug: "plot-twist",
-    videoUrl: ""
+    videoUrl: "https://youtu.be/orO74lGutB0"
   }
 ];
