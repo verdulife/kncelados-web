5706a7910062de9b8f889759ff5a0d6244b39d60
verdulife <verdu@live.com>
2025-07-30 17:59:16 +0200

    update to full shopify

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,1 @@
-const STORE_ID = "02b4e3ee-b56f-4d7d-a145-59cb3d73df74";
-
-export const STORE_ENDPOINTS = {
-  GET_STORE: `https://ecommerce.gelatoapis.com/v1/stores/${STORE_ID}/products`,
-  GET_PRODUCT: (id: string) => `https://ecommerce.gelatoapis.com/v1/stores/${STORE_ID}/products/${id}`,
-  GET_PRICE: (id: string, country: string, currency: string) => `https://product.gelatoapis.com/v3/products/${id}/prices?country=${country}&currency=${currency}`,
-};
+export const STORE_URL = "https://kncelados.myshopify.com";
